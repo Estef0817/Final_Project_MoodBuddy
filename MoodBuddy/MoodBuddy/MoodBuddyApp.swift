19d7ef95d619589eec49d13d225adbe7bcbfdcef
Estefany Mendez <estefanymendez@MacBookAir.lan>
2025-08-14 17:59:42 -0700

    created MoodBuddy

---
+++ MoodBuddy/MoodBuddy/MoodBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodBuddyApp.swift
+//  MoodBuddy
+//
+//  Created by Estefany Mendez on 8/14/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
